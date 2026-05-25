6201b7e6d9dd8a17bfe3287d26e97fd7d5de43fc
EVH2 <t000y20@163.com>
2026-05-26 06:59:11 +0800

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -13,7 +13,7 @@
   let API_BASE_URL;
   
   // 如果是 Cloudflare Pages 域名或 localhost
-  let API_BASE_URL = 'https://clawbot-api.xxxx.workers.dev';
+  let API_BASE_URL = 'https://3f76e9cd.bot-7fs.pages.dev';
   
   /**
    * 通用 API 请求函数
